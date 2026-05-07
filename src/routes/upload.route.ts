2004d038de382aa4e015c3d25c9a2d864ef2d0e8
pancharasshubham <pancharasshubham@gmail.com>
2026-05-07 19:31:08 +0530

    feat: add rate limiting to protect upload endpoint from abuse

--- src/routes/upload.route.ts
+++ src/routes/upload.route.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { handleUpload } from "../controllers/upload.controller";
 import { upload } from "../middlewares/upload.middleware";
+import { uploadRateLimiter } from "../middlewares/rate-limit.middleware"; 
 
 const router = Router();
 
@@ -8,5 +9,12 @@ router.post("/", (req, res, next) => {
   console.log("UPLOAD ROUTE HIT");
   next();
 }, upload.single("file"), handleUpload);
+
+router.post(
+  "/", 
+  uploadRateLimiter, 
+  upload.single("file"), 
+  handleUpload
+);
 
 export default router;
